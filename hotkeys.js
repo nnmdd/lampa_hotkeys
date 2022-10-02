eb89494a78a05d7917a104e55277617223e87ccb
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-02 15:18:06 +0500

    Add files via upload

---
+++ hotkeys.js
@@ -0,0 +1,36 @@
+Lampa.Platform.tv();
+  function isElementVisible(element) {
+            if (element.offsetWidth || 
+               element.offsetHeight || 
+               element.getClientRects().length)
+                return true;
+            else
+                return false;
+        }
+
+if (isElementVisible(document.querySelector('.player')) === true) {
+	document.addEventListener("keydown", function(inEvent){
+
+	  if (inEvent.keyCode === 48) {
+		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+		  document.querySelector('.player-panel__subs.button.selector').click();
+		} else {
+		  history.back();
+		}
+	  }
+	  if (inEvent.keyCode === 53) {
+		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+		  document.querySelector('.player-panel__playlist.button.selector').click();
+		} else {
+		  history.back();
+		}
+	  }
+	  if (inEvent.keyCode === 56) {
+		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+		  document.querySelector('.player-panel__tracks.button.selector').click();
+		} else {
+		  history.back();
+		}
+	  }
+	}
+});
