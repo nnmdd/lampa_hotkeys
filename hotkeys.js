84b85cbdf16b07417833f025ad4223a303d4cabd
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-02 17:58:51 +0500

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -1,41 +1,41 @@
-(function () {
-document.addEventListener("keydown", function(inEvent){	
+document.addEventListener("keydown", function(inEvent){
+log('Hotkeys', 'Hotkeys loaded');
 Lampa.Platform.tv();
-  function isElementVisible(element) {
-            if (element.offsetWidth || 
-               element.offsetHeight || 
-               element.getClientRects().length)
-                return true;
-            else
-                return false;
-        }
+function isElementVisible(element) {
+        if (element.offsetWidth || 
+            element.offsetHeight || 
+            element.getClientRects().length)
+            return true;
+        else
+            return false;
+    };
 
-if (isElementVisible(document.querySelector('.player')) == true) {
-	log('Hotkeys', 'player found');
-	  if (inEvent.keyCode === 48) {
-		log('Hotkeys', '0 pressed');
-		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
-		  document.querySelector('.player-panel__subs.button.selector').click();
-		} else {
-		  history.back();
-		}
-	  }
-	  if (inEvent.keyCode === 53) {
-		log('Hotkeys', '5 pressed');
-		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
-		  document.querySelector('.player-panel__playlist.button.selector').click();
-		} else {
-		  history.back();
-		}
-	  }
-	  if (inEvent.keyCode === 56) {
-		log('Hotkeys', '8 pressed');
-		if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
-		  document.querySelector('.player-panel__tracks.button.selector').click();
-		} else {
-		  history.back();
-		}
-	  }
+	if (isElementVisible(document.querySelector('player')) === true) {
+		log('Hotkeys', 'player found');
+
+		if (inEvent.keyCode === 48) {
+			log('Hotkeys', '0 pressed');
+			if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+			  document.querySelector('.player-panel__subs.button.selector').click();
+			} else {
+			  history.back();
+			}
+		  }
+		  if (inEvent.keyCode === 53) {
+			  log('Hotkeys', '5 pressed');
+			if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+			  document.querySelector('.player-panel__playlist.button.selector').click();
+			} else {
+			  history.back();
+			}
+		  }
+		  if (inEvent.keyCode === 56) {
+			  log('Hotkeys', '8 pressed');
+			if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
+			  document.querySelector('.player-panel__tracks.button.selector').click();
+			} else {
+			  history.back();
+			}
+		  }
 	}
-	}
-})();
+});
