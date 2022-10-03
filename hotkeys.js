491ee013879c788c35dfff0a0b9a83278723b1bd
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-03 05:02:08 +0500

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -2,7 +2,7 @@ Lampa.Platform.tv();
 function log() {
         console.log.apply(console.log, arguments);
       }
-log('Hotkeys', 'Hotkeys loaded');
+//log('Hotkeys', 'Hotkeys loaded');
 function listenDestroy() {
 	document.removeEventListener("keydown", listenHotkeys);
 	Lampa.Player.listener.remove('destroy', listenDestroy);	
@@ -27,7 +27,7 @@ function listenHotkeys(e) {
                 return false;
         }
   if (e.keyCode === 48) {
-    log('Hotkeys', '0 pressed');
+//    log('Hotkeys', '0 pressed');
     if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
       document.querySelector('.player-panel__subs.button.selector').click();
     } else {
@@ -35,7 +35,7 @@ function listenHotkeys(e) {
     }
   }
   if (e.keyCode === 53) {
-    log('Hotkeys', '5 pressed');
+//    log('Hotkeys', '5 pressed');
     if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
       document.querySelector('.player-panel__playlist.button.selector').click();
     } else {
@@ -43,7 +43,7 @@ function listenHotkeys(e) {
     }
   }
   if (e.keyCode === 56) {
-    log('Hotkeys', '8 pressed');
+//    log('Hotkeys', '8 pressed');
     if (isElementVisible(document.querySelector('.selectbox__layer')) === false) {
       document.querySelector('.player-panel__tracks.button.selector').click();
     } else {
