9e06d5755841c3e9ffc57924d4610b17cffc209f
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 04:07:39 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 <div id="co"> <!-- no onclick method needed -->
-<script>
+<script type="text/javascript">
+//<![CDATA[    
 $(document).ready(function() {
     $("#co").click(function() {
         $("body").css("background-color","black"); //edit, body must be in quotes!
     });
 });
 </script>
+//]]>
