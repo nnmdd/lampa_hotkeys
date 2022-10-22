d3e250800f9e81766afd8ae5088f82acc55c833b
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 04:16:54 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-Lampa.App.listener.follow('ready', changeBackground('black'));
-
-function changeBackground(color) {
-   document.body.style.background = color;
-}
-
-window.addEventListener("load",function() { changeBackground('black') });
+$(document.body).css( "background", "black" );
