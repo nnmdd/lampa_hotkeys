6af9f982f63905a1f70e2b4093d578916c1242c3
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 03:25:10 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 <script type='text/javascript'>
   document.getElementsByTagName('meta')["theme-color"].content = "#000000";
+  document.getElementsByTagName('meta')["msapplication-TileColor"].content = "#000000";
 </script>
