304a8e47f9179b9c8722f0ed04e18d8939f15ae3
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 03:48:15 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
+/*
 <script type='text/javascript'>
   document.getElementsByTagName('meta')["theme-color"].content = "#000000";
   document.getElementsByTagName('meta')["msapplication-TileColor"].content = "#000000";
 </script>
+*/
+/*
 <script type="text/javascript">
 var file = location.pathname.split( "/" ).pop();
-
 var link = document.createElement( "https://raw.githubusercontent.com/nnmdd/lampa_hotkeys/main/lampa-black.css" );
 link.href = file.substr( 0, file.lastIndexOf( "." ) ) + ".css";
 link.type = "text/css";
@@ -12,4 +14,8 @@ link.rel = "stylesheet";
 link.media = "screen,print";
 
 document.getElementsByTagName( "head" )[0].appendChild( link );
+</script>
+*/
+<script type="text/javascript">
+     document.body.style.backgroundColor = "#000";
 </script>
