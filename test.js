a0ef868fe43d3c464439a20201d43a8139b525d4
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 04:12:10 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-<div id="co"> <!-- no onclick method needed -->
 <script type="text/javascript">
-//<![CDATA[    
-$(document).ready(function() {
-    $("#co").click(function() {
-        $("body").css("background-color","black"); //edit, body must be in quotes!
-    });
-});
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
+window.addEventListener("load",function() { changeBackground('black') });
 </script>
-//]]>
