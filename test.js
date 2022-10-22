73856bbc446ac658a9c19fd2e4e6fafbab362ec6
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 03:58:12 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,3 @@
-<script type='text/javascript'>
-function setColor(element, color)
-{
-    element.style.backgroundColor = color;
-}
-
-// where el is the concerned element
-var el = document.getElementById('body');
-setColor(el, 'black');
+<script>
+$(document.body).css( "background", "black" );
 </script>
-/*
-<script type='text/javascript'>
-  document.getElementsByTagName('meta')["theme-color"].content = "#000000";
-  document.getElementsByTagName('meta')["msapplication-TileColor"].content = "#000000";
-</script>
-*/
-/*
-<script type="text/javascript">
-var file = location.pathname.split( "/" ).pop();
-var link = document.createElement( "https://raw.githubusercontent.com/nnmdd/lampa_hotkeys/main/lampa-black.css" );
-link.href = file.substr( 0, file.lastIndexOf( "." ) ) + ".css";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
-
-document.getElementsByTagName( "head" )[0].appendChild( link );
-</script>
-
-<script type="text/javascript">
-     document.body.style.backgroundColor = "#000";
-</script>
-*/
-
