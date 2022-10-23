8eb2d0b4bb0df7deb2d617918856ee56d7c62b96
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 06:02:43 +0500

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ document.addEventListener("keydown", listenFilter);
 function listenFilter(e) {
   if (e.keyCode === 49) {
   //    log('Hotkeys', '1 pressed');
-    log('Filter', this.selectedFilter);
+    log('Filter', Storage.get('torrents_filter', '{}'));
     }
 };
 listenFilter;
+log('Filter',  Storage.get('torrents_filter', '{}'));
