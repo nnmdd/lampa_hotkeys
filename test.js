c24c8f5694ebf231f93930234e5b164c1037b699
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 05:35:38 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-function log() {
-        console.log.apply(console.log, arguments);
-    
-$(document.body).css( "background", "black" );
-
 log('Filter', this.selectedFilter);
-log('Filter', this.selectedFilter());
-this.selectedFilter();
