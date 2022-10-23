42d28244f39e250657085d0fce3ceb1936c760f8
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 05:13:23 +0500

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,8 @@
+function log() {
+        console.log.apply(console.log, arguments);
+    
+$(document.body).css( "background", "black" );
+
+log('Filter', this.selectedFilter);
+log('Filter', this.selectedFilter());
+this.selectedFilter();
