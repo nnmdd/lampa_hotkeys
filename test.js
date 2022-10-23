e1ad729178a1326a2dc3473d44d4f705170c69a5
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-10-23 05:51:37 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-function listenHotkeys(e) {
-if (e.keyCode === 49) {
-//    log('Hotkeys', '1 pressed');
-  log('Filter', this.selectedFilter);
+document.addEventListener("keydown", listenFilter);
+function listenFilter(e) {
+  if (e.keyCode === 49) {
+  //    log('Hotkeys', '1 pressed');
+    log('Filter', this.selectedFilter);
     }
 };
-
-
+listenFilter();
