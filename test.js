677e39cff8d3bb39a129e04c5c636498cf571c53
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-11-04 05:11:26 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-document.addEventListener("keydown", listenFilter);
-function listenFilter(e) {
-  if (e.keyCode === 49) {
-  //    log('Hotkeys', '1 pressed');
-    log('Filter', Storage.get('torrents_filter', '{}'));
-    }
-};
-listenFilter;
-log('Filter',  Storage.get('torrents_filter', '{}'));
+filter.onCheck = (type, a, b)=>{
+            let data = Lampa.Storage.get('torrents_filter','{}'),
+                need = Lampa.Arrays.toArray(data[a.stype])
+
+            if(b.checked && need.indexOf(b.title)) need.push(b.title)
+            else if(!b.checked) Lampa.Arrays.remove(need, b.title)
+
+            data[a.stype] = need
+
+            Lampa.Storage.set('torrents_filter',data)
+
+            a.subtitle = need.join(', ')
+
+            this.applyFilter()
+        }
