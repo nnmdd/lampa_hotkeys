9b481de5cf8cd2cb45d8f02e55778050eceadbfb
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-11-11 17:43:07 +0500

    Update lampa-black.js

--- lampa-black.js
+++ lampa-black.js
@@ -2,8 +2,13 @@ function makeNoise() {
   $(document.body).css( "background", "black" );
   $(".selectbox__content").css( "background", "black" );
   $(".settings__content").css( "background", "black" );
+  
+  $(".settings-param.focus").css( "background-color", "white" );
+  $(".settings-param.focus").css( "color", "black" );
+  
   $("li.menu__item.selector").css( "-webkit-border-radius", "0" );
   $("li.menu__item.selector").css( "-moz-border-radius", "0" );
   $("li.menu__item.selector").css( "border-radius", "0" );
+
 };
 Lampa.Listener.follow('app', makeNoise);
