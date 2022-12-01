f053af50cd1e7043ebd8652ad9da0b17a47f6b1f
nnmdd <97770937+nnmdd@users.noreply.github.com>
2022-12-02 03:25:43 +0500

    Delete test2.js

--- test2.js
+++
@@ -1,8 +0,0 @@
-function log() {
-        console.log.apply(console.log, arguments);
-    
-$(document.body).css( "background", "black" );
-
-log('Filter', this.selectedFilter);
-log('Filter', this.selectedFilter());
-this.selectedFilter();
