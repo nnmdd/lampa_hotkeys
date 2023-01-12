b88b42d4a8d4ed9f293a7da8bf4651b00c4778af
nnmdd <97770937+nnmdd@users.noreply.github.com>
2023-01-13 03:00:17 +0500

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -16,7 +16,7 @@ function listenHotkeys(e) {
 	
   if (e.keyCode === 48) {
     log('Hotkeys', '0 pressed');
-    if document.querySelector('.selectbox--open') == undefined) {
+    if (document.querySelector('.selectbox--open') == undefined) {
       document.querySelector('.player-panel__subs.button.selector').click();
     } else {
       history.back();
@@ -24,7 +24,7 @@ function listenHotkeys(e) {
   }
   if (e.keyCode === 53) {
     log('Hotkeys', '5 pressed');
-    if document.querySelector('.selectbox--open') == undefined) {
+    if (document.querySelector('.selectbox--open') == undefined) {
       document.querySelector('.player-panel__playlist.button.selector').click();
     } else {
       history.back();
@@ -32,7 +32,7 @@ function listenHotkeys(e) {
   }
   if (e.keyCode === 56) {
     log('Hotkeys', '8 pressed');
-    if document.querySelector('.selectbox--open') == undefined) {
+    if (document.querySelector('.selectbox--open') == undefined) {
       document.querySelector('.player-panel__tracks.button.selector').click();
     } else {
       history.back();
