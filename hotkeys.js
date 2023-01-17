e10abe3335585fe2a45910aa2917b444bfb905a4
nnmdd <97770937+nnmdd@users.noreply.github.com>
2023-01-17 23:03:00 +0500

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -18,7 +18,7 @@ function listenHotkeys(e) {
     log('Hotkeys', '0 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	log('Hotkeys', 'subs list not visible');
-	document.querySelector('.player-panel__subs.button.selector').click();
+	//document.querySelector('.player-panel__subs.button.selector').click();
 	Lampa.Utils.trigger(document.querySelector('.player-panel__subs.button.selector'), 'click');
     } else {
       history.back();
@@ -28,7 +28,7 @@ function listenHotkeys(e) {
     log('Hotkeys', '5 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	log('Hotkeys', 'playlist not visible');
-	document.querySelector('.player-panel__playlist.button.selector').click();
+	//document.querySelector('.player-panel__playlist.button.selector').click();
 	Lampa.Utils.trigger(document.querySelector('.player-panel__playlist.button.selector'), 'click');
     } else {
       history.back();
@@ -38,7 +38,7 @@ function listenHotkeys(e) {
     log('Hotkeys', '8 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	log('Hotkeys', 'audio list not visible');
-	document.querySelector('.player-panel__tracks.button.selector').click();
+	//document.querySelector('.player-panel__tracks.button.selector').click();
 	Lampa.Utils.trigger(document.querySelector('.player-panel__tracks.button.selector'), 'click');
     } else {
       history.back();
