7c1fc3abbe492ddf28dc3333e5e0344aa9b7d0e5
nnmdd <97770937+nnmdd@users.noreply.github.com>
2023-01-19 05:00:08 +0500

    Update hotkeys.js

--- hotkeys.js
+++ hotkeys.js
@@ -15,37 +15,32 @@ function startHotkeys() {
 };
 
 function listenHotkeys(e) {
-	
-  if (e.keyCode === 48 || e.keyCode === 96) {
-    //log('Hotkeys', '0 pressed');
-    if (!document.querySelector('body.selectbox--open')) {
-	//log('Hotkeys', 'subs list not visible');
-	//document.querySelector('.player-panel__subs.button.selector').click();
-	Lampa.Utils.trigger(document.querySelector('.player-panel__subs.button.selector'), 'click');
-    } else {
-      history.back();
-    }
-  }
-  if (e.keyCode === 53 || e.keyCode === 101) {
-    //log('Hotkeys', '5 pressed');
-    if (!document.querySelector('body.selectbox--open')) {
-	//log('Hotkeys', 'playlist not visible');
-	//document.querySelector('.player-panel__playlist.button.selector').click();
-	Lampa.Utils.trigger(document.querySelector('.player-panel__playlist.button.selector'), 'click');
-    } else {
-      history.back();
-    }
-  }
-  if (e.keyCode === 56 || e.keyCode === 104) {
-    //log('Hotkeys', '8 pressed');
-    if (!document.querySelector('body.selectbox--open')) {
-	//log('Hotkeys', 'audio list not visible');
-	//document.querySelector('.player-panel__tracks.button.selector').click();
-	Lampa.Utils.trigger(document.querySelector('.player-panel__tracks.button.selector'), 'click');
-    } else {
-      history.back();
-    }
-  }
+	if (e.keyCode === 48 || e.keyCode === 96) {
+		//log('Hotkeys', '0 pressed');
+    		if (!document.querySelector('body.selectbox--open')) {
+			//log('Hotkeys', 'subs list not visible');
+			Lampa.Utils.trigger(document.querySelector('.player-panel__subs.button.selector'), 'click');
+		} else {
+		      history.back();
+    		}
+	}
+	if (e.keyCode === 53 || e.keyCode === 101) {
+  		//log('Hotkeys', '5 pressed');
+		if (!document.querySelector('body.selectbox--open')) {
+			//log('Hotkeys', 'playlist not visible');
+			Lampa.Utils.trigger(document.querySelector('.player-panel__playlist.button.selector'), 'click');
+    		} else {
+      			history.back();
+    		}
+  	}
+  	if (e.keyCode === 56 || e.keyCode === 104) {
+    		//log('Hotkeys', '8 pressed');
+    		if (!document.querySelector('body.selectbox--open')) {
+			//log('Hotkeys', 'audio list not visible');
+			Lampa.Utils.trigger(document.querySelector('.player-panel__tracks.button.selector'), 'click');
+    		} else {
+      			history.back();
+		}
+  	}
 };
-
 Lampa.Player.listener.follow('ready',startHotkeys);
