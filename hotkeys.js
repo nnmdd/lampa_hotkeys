de9afe64273dcc0f7935afff12958f0f552b25f2
nnmdd <97770937+nnmdd@users.noreply.github.com>
2024-05-14 21:50:30 +0500

    Update hotkeys.js
    added Tv lg *UJ634V-ZD
    WebOS: 3.9.0

--- hotkeys.js
+++ hotkeys.js
@@ -27,16 +27,16 @@ function startHotkeys() {
 function listenHotkeys(e) {
 
 //log('Hotkeys', e.keyCode);
-	
-  if (e.keyCode === 166 || e.keyCode === 427 || e.keyCode === 27 || e.keyCode === 33) {
+//Channel Up	
+  if (e.keyCode === 166 || e.keyCode === 427 || e.keyCode === 27 || e.keyCode === 33 || e.keyCode === 402) {
 	openPanel('.player-panel__next.button.selector');
   }
-
-  if (e.keyCode === 167 || e.keyCode === 428 || e.keyCode === 28 || e.keyCode === 34) {
+//Channel Down
+  if (e.keyCode === 167 || e.keyCode === 428 || e.keyCode === 28 || e.keyCode === 34 || e.keyCode === 403) {
 	openPanel('.player-panel__prev.button.selector');
   }
-	
-  if (e.keyCode === 48 || e.keyCode === 96) {
+//0	
+  if (e.keyCode === 48 || e.keyCode === 96 || e.keyCode === 11) {
     //log('Hotkeys', '0 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	//log('Hotkeys', 'subs list not visible');
@@ -45,7 +45,8 @@ function listenHotkeys(e) {
       	history.back();
     }
   }
-  if (e.keyCode === 53 || e.keyCode === 101) {
+//5	
+  if (e.keyCode === 53 || e.keyCode === 101 || e.keyCode === 6) {
     //log('Hotkeys', '5 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	//log('Hotkeys', 'playlist not visible');
@@ -54,7 +55,8 @@ function listenHotkeys(e) {
       	history.back();
     }
   }
-  if (e.keyCode === 56 || e.keyCode === 104) {
+//8	
+  if (e.keyCode === 56 || e.keyCode === 104 || e.keyCode === 9) {
     //log('Hotkeys', '8 pressed');
     if (!document.querySelector('body.selectbox--open')) {
 	//log('Hotkeys', 'audio list not visible');
